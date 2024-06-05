c5780d3fd23a83e9ccf5593768e4d2fe1044bd22
JakovM <jm52768@fer.hr>
2024-06-06 00:17:33 +0200

    added basic components

--- front/palacinke/src/app/app.component.ts
+++ front/palacinke/src/app/app.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from "./components/header/header.component";
+import { PlantCardsComponent } from "./components/plant-cards/plant-cards.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [CommonModule, RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css'],
+    imports: [CommonModule, RouterOutlet, HeaderComponent, PlantCardsComponent]
 })
 export class AppComponent {
   title = 'palacinke';
---
+++ front/palacinke/src/app/components/plant-cards/plant-cards.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-plant-cards',
+  standalone: true,
+  imports: [],
+  templateUrl: './plant-cards.component.html',
+  styleUrl: './plant-cards.component.css'
+})
+export class PlantCardsComponent {
+
+}
